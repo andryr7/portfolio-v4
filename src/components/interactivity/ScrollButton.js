5920fdda8cb39670fb2df1de4e1c03dfef09e6fa
unknown <andryratsimba@gmail.com>
2024-01-12 12:40:17 +0100

    fixed scroll button behavior and changed delay

--- src/components/interactivity/ScrollButton.js
+++ src/components/interactivity/ScrollButton.js
@@ -1,7 +1,7 @@
 import { PortfolioContext } from "@/utils/Context";
 import { useCallback, useContext } from "react";
-import { styled, useTheme, keyframes } from "styled-components"
-import { useLenis } from "@studio-freight/react-lenis"
+import { styled, useTheme, keyframes } from "styled-components";
+import { useLenis } from "@studio-freight/react-lenis";
 
 const flash = keyframes`
   from {
@@ -12,25 +12,32 @@ const flash = keyframes`
     cursor: pointer;
     opacity: 1;
   }
-`
+`;
 
 const StyledSvg = styled.svg`
   opacity: 0;
-  animation: ${flash} 2s ease 7s infinite;
-`
+  animation: ${flash} 2s ease 3.5s infinite;
+`;
 
 export default function ScrollButton() {
-  const { aboutSectionRef } = useContext(PortfolioContext);
+  const { workSectionRef } = useContext(PortfolioContext);
   const theme = useTheme();
   const lenis = useLenis();
 
   const handleClick = useCallback(() => {
-    lenis.scrollTo(aboutSectionRef.current, {lerp: 0.05});
-  },[lenis, aboutSectionRef]);
+    lenis.scrollTo(workSectionRef.current, { lerp: 0.05 });
+  }, [lenis, workSectionRef]);
 
-  return(
-    <StyledSvg xmlns="http://www.w3.org/2000/svg" fill={theme.main} height="48" viewBox="0 -960 960 960" width="48" onClick={handleClick}>
-      <path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z"/>
+  return (
+    <StyledSvg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={theme.main}
+      height="48"
+      viewBox="0 -960 960 960"
+      width="48"
+      onClick={handleClick}
+    >
+      <path d="M480-345 240-585l43-43 197 198 197-197 43 43-240 239Z" />
     </StyledSvg>
-  )
+  );
 }
